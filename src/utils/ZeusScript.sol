53149a01e74f9dcb9191cd1a6d93d45cca01f561
Nadir Akhtar <nadirakhtar123@gmail.com>
2024-10-31 10:33:07 -0700

    feat: add zeusTest

--- src/utils/ZeusScript.sol
+++ src/utils/ZeusScript.sol
@@ -4,11 +4,16 @@ pragma solidity ^0.8.12;
 import {StringUtils} from "./StringUtils.sol";
 import {Script} from "forge-std/Script.sol";
 
-contract ZeusScript is Script {
+abstract contract ZeusScript is Script {
     using StringUtils for string;
 
     string internal constant addressPrefix = "ZEUS_DEPLOYED_";
     string internal constant envPrefix = "ZEUS_ENV_";
+
+    /**
+     * @notice A test function to be implemented by the inheriting contract.
+     */
+    function zeusTest() public virtual;
 
     /**
      * @notice Returns the address of a contract based on the provided key, querying the envvars injected by Zeus. This is typically the name of the contract.
