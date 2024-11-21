7c96dc94fa1557f0d9b24a61674c5d1c708cd525
Justin Brower <jbrower95@gmail.com>
2024-11-21 15:13:50 -0500

    save

--- src/templates/MultisigBuilder.sol
+++ src/templates/MultisigBuilder.sol
@@ -18,23 +18,16 @@ abstract contract MultisigBuilder is ZeusScript {
      * @notice Constructs a SafeTx object for a Gnosis Safe to ingest. Emits via `ZeusMultisigExecute`
      */
     function execute() public {
-        // get calls for Multisig from inheriting script
-        MultisigCall[] memory calls = _execute();
-
-        // encode calls as MultiSend data
-        bytes memory data = calls.encodeMultisendTxs();
-
-        // creates and return SafeTx object
-        // assumes 0 value (ETH) being sent to multisig
-
-        address multiSendCallOnly = zAddress(multiSendCallOnlyName);
-
-        emit ZeusMultisigExecute(multiSendCallOnly, 0, data, EncGnosisSafe.Operation.DelegateCall);
+        vm.startPrank(zAddress("MULTISIG"));
+        runAsMultisig();
+        vm.stopPrank();
     }
 
     /**
      * @notice To be implemented by inheriting contract.
-     * @return An array of MultisigCall objects.
+     * 
+     * This function will be pranked from the perspective of the multisig you choose to run with.
+     * DO NOT USE vm.startPrank()/stopPrank() during your implementation.
      */
-    function _execute() internal virtual returns (MultisigCall[] memory);
+    function runAsMultisig() internal virtual;
 }
